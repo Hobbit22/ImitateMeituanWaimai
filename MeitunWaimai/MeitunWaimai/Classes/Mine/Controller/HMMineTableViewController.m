6f60073cc12410aff77e414872de2f7bad6dc1c6
胡碧涛 <2076917843@qq.com>
2017-04-28 10:07:42 +0800

    修改了什么什么

--- MeitunWaimai/MeitunWaimai/Classes/Mine/Controller/HMMineTableViewController.m
+++ MeitunWaimai/MeitunWaimai/Classes/Mine/Controller/HMMineTableViewController.m
@@ -8,7 +8,7 @@
 
 #import "HMMineTableViewController.h"
 
-@interface HMMineTableViewController ()
+@interface HMMineTableViewController ()<UITableViewDelegate>
 
 @end
 
@@ -24,32 +24,27 @@
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Incomplete implementation, return the number of sections
+
     return 0;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete implementation, return the number of rows
+
     return 0;
 }
 
-/*
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"" forIndexPath:indexPath];
     
     // Configure the cell...
     
     return cell;
 }
-*/
+
 
 /*
 // Override to support conditional editing of the table view.
